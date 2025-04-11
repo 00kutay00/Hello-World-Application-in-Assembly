c8faea9f437b3ffe637e727f11ab7bf3e9730171
Kutay Ertuğ Ateş <77014041+00kutay00@users.noreply.github.com>
2025-04-11 03:24:01 +0300

    Create hello.asm

---
+++ hello.asm
@@ -0,0 +1,17 @@
+section .data
+  msg db "Hello World From Assembly!", 0
+  len equ $ - msg
+
+section .text
+  global _start
+
+_start:
+  mov rax, 1      ; Syscall Number | 1 = write
+  mov rdi, 1      ; Parameter 1 | 1 = stdout
+  mov rsi, msg    ; Parameter 2 | msg adress
+  mov rdx, len    ; Parameter 3 | length
+  syscall
+
+  mov rax, 60     ; Syscall Number | 60 = exit
+  xor rdi, rdi    ; reset rdi register
+  syscall 
